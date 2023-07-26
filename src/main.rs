21b932b9b067d165ae9c817890db33500cf98180
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-26 21:35:26 +0530

    add input string parse to int logic.

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+    let guess: u32 = guess.trim().parse().expect("Type a number");
     println!("You guessed: {}", guess);
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too small!"),
