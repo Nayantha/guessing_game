2d560c7445050d1a2656670b67c7b0c61c24ee42
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-26 21:50:52 +0530

    remove test printing statements.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::io;
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..101);
-    println!("The secret number is: {}", secret_number);
+    // println!("The secret number is: {}", secret_number);
     loop {
         println!("Please input your guess.");
         let mut guess = String::new();
@@ -19,7 +19,7 @@ fn main() {
                 continue;
             }
         };
-        println!("You guessed: {}", guess);
+        // println!("You guessed: {}", guess);
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
